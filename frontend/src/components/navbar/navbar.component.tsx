de95282bbe392beaf1b46ecada59267260491c4a
BogdanDirtyDwarf <i21s045@students.it-college.ru>
2022-11-20 17:59:38 +0500

    [FEATURE] Created navbar component

---
+++ frontend/src/components/navbar/navbar.component.tsx
@@ -0,0 +1,5 @@
+import React, { FunctionComponent } from "react";
+
+export const NavbarComponent: FunctionComponent = () => {
+  return (<div>This is Navigation</div>);
+};
--- frontend/src/views/main/main.view.tsx
+++ frontend/src/views/main/main.view.tsx
@@ -1,6 +1,10 @@
 import React, { FunctionComponent } from "react";
 import s from "./main.module.scss";
+import * as Component from "../../components/export";
 
 export const MainView: FunctionComponent = () => (
-  <section>Main view</section>
+  <>
+    <Component.NavbarComponent />
+    <section>Main view</section>
+  </>
 );
