ad49d930080ab876bb51b8c6f8ae26effec92a08
dmitryqv <vlasenko_dima@icloud.com>
2022-11-20 17:45:44 +0500

    [CHANGE]

--- frontend/src/views/main/main.view.tsx
+++ frontend/src/views/main/main.view.tsx
@@ -1,6 +1,8 @@
 import React, { FunctionComponent } from "react";
 import s from "./main.module.scss";
 
-export const MainView: FunctionComponent = () => (
-  <section>Main view</section>
-);
+export const MainView: FunctionComponent = () => {
+  return (
+    <section>Main view</section>
+  );
+};
