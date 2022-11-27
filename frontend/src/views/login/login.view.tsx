3c747744f943cda1323c36330948e1b218adba48
dmitryqv <vlasenko_dima@icloud.com>
2022-11-27 14:47:57 +0500

    [FEATURE] Added Fonts / Fix login layout

--- frontend/src/views/login/login.view.tsx
+++ frontend/src/views/login/login.view.tsx
@@ -8,26 +8,28 @@ export const LoginView: FunctionComponent = () => {
   const payload = useTypedSelector((state) => state.auth);
 
   return (
-    <section className={s.form}>
-      <input
-        className={s.input}
-        type="email"
-        placeholder="Почта"
-        onInput={(e: ChangeEvent<HTMLInputElement>) =>
-          changeEmail(e.target.value)
-        }
-      />
-      <input
-        className={s.input}
-        type="password"
-        placeholder="Пароль"
-        onInput={(e: ChangeEvent<HTMLInputElement>) =>
-          changePassword(e.target.value)
-        }
-      />
-      <button className={s.button} onClick={() => console.log(payload)}>
-        Войти
-      </button>
+    <section className={s.form__layout}>
+      <section className={s.form}>
+        <input
+          className={s.input}
+          type="email"
+          placeholder="Почта"
+          onInput={(e: ChangeEvent<HTMLInputElement>) =>
+            changeEmail(e.target.value)
+          }
+        />
+        <input
+          className={s.input}
+          type="password"
+          placeholder="Пароль"
+          onInput={(e: ChangeEvent<HTMLInputElement>) =>
+            changePassword(e.target.value)
+          }
+        />
+        <button className={s.button} onClick={() => console.log(payload)}>
+          Войти
+        </button>
+      </section>
     </section>
   );
 };
