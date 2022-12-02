80958a405b7c5d60943036ccc6cb22ce0b6e3183
dmitryqv <vlasenko_dima@icloud.com>
2022-12-02 18:01:26 +0500

    Added poster

--- frontend/src/views/main/main.view.tsx
+++ frontend/src/views/main/main.view.tsx
@@ -5,7 +5,21 @@ import * as Layout from "../../layout/responsive/responsive.layout";
 export const MainView: FunctionComponent = () => {
   return (
     <Layout.ResponsiveLayout>
-      <section>Main view</section>
+      <section className={s.main}>
+        <article className={s.poster}>
+          {/* <div className={s.poster__layout}>
+            <img className={s.avatar}
+            //  src={require("../../img/avatar.jpg")}
+              />
+            <h3>centenario</h3>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem
+              alias ipsum porro magni eligendi repellat optio dolorum nemo enim
+              sequi laboriosam
+            </p>
+          </div> */}
+        </article>
+      </section>
     </Layout.ResponsiveLayout>
   );
 };
