07ea5a6127807088451ed557271b82ee3a362da6
BogdanDirtyDwarf <i21s045@students.it-college.ru>
2022-12-03 00:36:49 +0500

    [FEATURE] Created modal

--- frontend/src/modals/search/search.modal.tsx
+++ frontend/src/modals/search/search.modal.tsx
@@ -1,9 +1,10 @@
 import React, { FunctionComponent } from "react";
+import * as Layout from "../../layout/responsive/responsive.layout";
 
 export const SearchModal: FunctionComponent = () => {
   return (
-    <>
+    <Layout.ResponsiveLayout>
       <div>Modal content</div>
-    </>
+    </Layout.ResponsiveLayout>
   );
 };
