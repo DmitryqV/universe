99c9b025cd135d6c139b1b66e8445718305ab394
BogdanDirtyDwarf <i21s045@students.it-college.ru>
2022-12-08 18:06:46 +0500

    [UI] Fixed navbar popup show

--- frontend/src/components/navbar__popup/navbar__popup.component.tsx
+++ frontend/src/components/navbar__popup/navbar__popup.component.tsx
@@ -1,19 +1,14 @@
 import React, { FunctionComponent } from "react";
 import s from "./navbar__popup.module.scss";
 import { useTypedSelector } from "@hooks/redux.useTypedSelector";
-import { useActions } from "@hooks/redux.useActions";
 
 export const NavbarPopup: FunctionComponent = () => {
-  const { NavbarPopupChangeNavbarPopUpShow } = useActions();
   const payload = useTypedSelector((state) => state.NavbarPopup);
 
   return (
     <>
       {payload.show && (
-        <ul
-          onMouseLeave={() => NavbarPopupChangeNavbarPopUpShow(false)}
-          className={s.popup}
-        >
+        <ul className={s.popup}>
           <li className={s.popup__item}>Чаты</li>
           <li className={s.popup__item}>Настройки</li>
           <li className={s.popup__item}>Выйти</li>
