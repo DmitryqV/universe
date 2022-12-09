1d3d7015bfe040faab5151dcd36242874d8d5d62
DmitryqV <vlasenko_dima@icloud.com>
2022-12-09 23:55:58 +0500

    removed hash router

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,19 +1,19 @@
 import React, { FunctionComponent } from "react";
 import "@ui/null.module.scss";
 
-import { Routes, Route, HashRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import * as View from "@views/export";
 import { Provider } from "react-redux";
 import { store } from "@store/store";
 
 const AppRouting: FunctionComponent = () => (
-  <HashRouter>
+  <BrowserRouter>
     <Routes>
       <Route path="/" element={<View.Login />} />
       <Route path="/@:username" element={<View.Main />} />
       <Route path="*" element={<View.Error />} />
     </Routes>
-  </HashRouter>
+  </BrowserRouter>
 );
 
 export const AppCore: FunctionComponent = () => (
