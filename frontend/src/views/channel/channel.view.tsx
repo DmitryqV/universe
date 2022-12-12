32007f47fe415024ff937808468bfbe2bf094099
DmitryqV <vlasenko_dima@icloud.com>
2022-12-13 02:02:49 +0500

    [FEATURE] Create channel view

---
+++ frontend/src/views/channel/channel.view.tsx
@@ -0,0 +1,10 @@
+import React, { FunctionComponent } from "react";
+import { Layout } from "@layouts/export";
+
+export const Channel: FunctionComponent = () => {
+  return (
+    <Layout.Responsive>
+      <section>channel view</section>
+    </Layout.Responsive>
+  );
+};
