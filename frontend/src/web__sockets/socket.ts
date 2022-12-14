5d8ccee0af0ce030146c5676b72b4cf58d528a89
i21s692 <i21s692@students.it-college.ru>
2022-12-14 23:39:23 +0500

    [REFACTORING] code style

--- frontend/src/web__sockets/socket.ts
+++ frontend/src/web__sockets/socket.ts
@@ -1,7 +1,10 @@
-import { io, Socket } from 'socket.io-client';
+import { io, Socket } from "socket.io-client";
 
-export const socket: Socket = io(process.env.NODE_ENV === 'production' ? '/' : 'http://localhost:81/', {
-  forceNew: true,
-  timeout: 1000,
-  transports: ['websocket'],
-});
+export const socket: Socket = io(
+  process.env.NODE_ENV === "production" ? "/" : "http://localhost:81/",
+  {
+    forceNew: true,
+    timeout: 1000,
+    transports: ["websocket"],
+  }
+);
