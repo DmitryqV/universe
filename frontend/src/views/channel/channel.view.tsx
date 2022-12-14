4679f09f31d871974ff8ed75bd0e8f4a201afa4b
DmitryqV <vlasenko_dima@icloud.com>
2022-12-14 21:27:49 +0500

    [UI] Create channel view and channel stream card

--- frontend/src/views/channel/channel.view.tsx
+++ frontend/src/views/channel/channel.view.tsx
@@ -1,10 +1,24 @@
 import React, { FunctionComponent } from "react";
 import { Layout } from "@layouts/export";
+import s from "./channel.module.scss";
 
 export const Channel: FunctionComponent = () => {
   return (
-    <Layout.Responsive>
-      <section>channel view</section>
-    </Layout.Responsive>
+    <section className={s.channel__backdrop}>
+      <Layout.Responsive>
+        <section className={s.channel__wrapper}>
+          <section className={s.channel__list}>
+            <div className={s.channel__stream}>
+              <div className={s.channel__stream__avatar}></div>
+              <p className={s.channel__stream__user}>@centenario</p>
+            </div>
+            <div className={s.channel__stream}>
+              <div className={s.channel__stream__avatar}></div>
+              <p className={s.channel__stream__user}>@centenario</p>
+            </div>
+          </section>
+        </section>
+      </Layout.Responsive>
+    </section>
   );
 };
